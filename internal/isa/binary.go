ecfcf0b778a8149140b972b7e7d87182e7dc49c5
Naswek <nastya.rovkova06@gmail.com>
2026-07-23 01:40:00 +0300

    add: first version of binary encode and decode

---
+++ internal/isa/binary.go
@@ -0,0 +1,72 @@
+package isa
+
+import (
+	"encoding/binary"
+	"log"
+	"os"
+)
+
+func ReadProgram(filename string) ([]uint32, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return fromByteToUint32(data), nil
+}
+
+func WriteProgram(filename string, data []uint32) error {
+	bytes := fromUint32ToByte(data)
+	err := os.WriteFile(filename, bytes, 0644)	
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
+
+func EncodeProgram(instrs []Instruction) []uint32 {
+	program := []uint32{}
+	for _, instr := range instrs {
+		program = append(program, EncodeInstruction(instr))
+	}
+	return program
+}
+
+func DecodeProgram(program []uint32) []Instruction {
+	instrs := []Instruction{}
+	for _, instr := range program {
+		instrs = append(instrs, DecodeInstruction(instr))
+	}
+	return instrs
+}
+
+
+func EncodeInstruction(instr Instruction) uint32 {
+	return uint32(instr.Opcode)<<24 | uint32(instr.Operand & 0xFFFFFF)
+}
+
+func DecodeInstruction(instr uint32) Instruction {
+	return Instruction{
+		Opcode:  Opcode(instr >> 24),
+		Operand: Operand(instr & 0xFFFFFF),
+	}
+}
+
+func fromUint32ToByte(data []uint32) []byte {
+	result := make([]byte, len(data)*4)
+
+	for i, number := range result {
+		binary.BigEndian.PutUint32(result[i*4:], uint32(number))
+	}
+
+	return result
+}
+
+func fromByteToUint32(data []byte) []uint32 {
+	result := make([]uint32, len(data)/4)
+
+	for i:= range result {
+		result[i] = binary.BigEndian.Uint32(data[i*4 : (i+1)*4])
+	}
+
+	return result
+}

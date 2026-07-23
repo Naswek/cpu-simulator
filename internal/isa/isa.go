b1cbc9f98edff63b18ce6ca5957611eded6a694c
Naswek <nastya.rovkova06@gmail.com>
2026-07-23 20:50:45 +0300

    update: isa, add register, status and more enums

--- internal/isa/isa.go
+++ internal/isa/isa.go
@@ -1,19 +1,53 @@
 package isa
 
-type Opcode uint8
-type Operand uint32
-type Register string
-
 const (
 	WordSize uint32 = 4
 	MemSize  uint32 = 2048
 	DataStackSize uint32 = MemSize
 	ReturnStackSize uint32 = 1500
 	IOPortCount uint32 = 4 
+	PORT_IN = 0
+	PORT_OUT = 1
+	InterruptVectorInput = 0
+	InterruptVectorOutput = 1
+)
+
+
+type Register uint32
+const (
+	ACC Register = iota
+	PC
+	IR
+	DSP
+	RS
+	SR
+	AR
+	DR
 )
 
 
 
+type Status uint8
+const (
+	Z Status = iota
+	N
+	C
+	V
+	IE
+	IM
+)
+
+type Opcode uint8
+type OpcodeKind uint8
+const (
+	none OpcodeKind = iota
+	imm
+	addr
+	stack_offset
+	port
+)
+
+type Operand uint32
 const (
 	HALT = 0x01
 	NOP  = 0x02
@@ -23,23 +57,24 @@ const (
 
 	JMP = 0x10
 	JZ	= 0x11
-	JN  = 0x12
-	JNZ = 0x13
-	CALL = 0x14
-	CALL_IND = 0x15
-	RET = 0x16
-	PUSH = 0x17
-	POP = 0x18
+	JС  = 0x12
+	JNС = 0x13
+	JP = 0x14
+	JN = 0x15
+	JNZ = 0x16
+	CALL = 0x17
+	CALL_IND = 0x18
+	RET = 0x19
+	PUSH = 0x1A
+	POP = 0x1B
 
 	LD_IMM = 0x20
 	LD_ADDR = 0x21
-	LD_SP = 0x22
-	LD_IND = 0x23
-	LD_SP_N = 0x24
-	ST_ADDR = 0x25
-	ST_SP = 0x26
-	ST_IND = 0x27
-	ST_SP_N = 0x28
+	LD_IND = 0x22
+	LD_SP_N = 0x23
+	ST_ADDR = 0x24
+	ST_IND = 0x25
+	ST_SP_N = 0x26
 
 	ADD = 0x30
 	SUB = 0x31
@@ -59,70 +94,60 @@ const (
 
 type OpcodeInfo struct {
 	Mnemonic 	string
-	HasOperant 	bool
+	OperandKind OpcodeKind
 	Ticks		int
 }
 
-type Operand struct {
-	Value int
-}
 
 var OpcodeTable = map[Opcode]OpcodeInfo {
-	HALT : {"halt", false, },
-	NOP	 : {"nop", false, },
-	EI	 : {"ei", false, },
-	DI   : {"di", false, },
-	IRET : {"iret", false, },
+	HALT : {"halt", none, },
+	NOP	 : {"nop", none, },
+	EI	 : {"ei", none, },
+	DI   : {"di", none, },
+	IRET : {"iret", none, },
 
-	JMP  : {"jmp", true, },
-	JZ   : {"jz", true, },
-	JN 	 : {"jn", true, },
-	JNZ	 : {"jnz", true, },
-	CALL : {"call", true, },
-	CALL_IND : {"call_ind", false, },
-	RET : {"ret", false, },
+	JMP  : {"jmp", addr, },
+	JZ   : {"jz", addr, },
+	JN 	 : {"jn", addr, },
+	JNZ	 : {"jnz", addr, },
+	CALL : {"call", addr, },
+	JP	 : {"jp", addr, },
+	JC	 : {"jc", addr, },
+	CALL_IND : {"call_ind", none, },
+	RET : {"ret", none, },
 
-	PUSH : {"push", false, },
-	POP  : {"pop", false, },
+	PUSH : {"push", none, },
+	POP  : {"pop", none, },
 
-	LD_IMM  : {"ld_imm", true, },
-	LD_ADDR : {"ld_addr", true, },
-	LD_SP   : {"ld_sp", true, },
-	LD_IND  : {"ld_ind", true, },
-	LD_SP_N : {"ld_sp_n", true, },
-	ST_ADDR : {"st_addr", true, },
-	ST_SP   : {"st_sp", true, },
-	ST_IND  : {"st_ind", true, },
-	ST_SP_N : {"st_sp_n", true, },
+	LD_IMM  : {"ld_imm", imm, },
+	LD_ADDR : {"ld_addr", addr, },
+	LD_IND  : {"ld_ind", addr, },
+	LD_SP_N : {"ld_sp_n", stack_offset, },
+	ST_ADDR : {"st_addr", addr, },
+	ST_IND  : {"st_ind", addr, },
+	ST_SP_N : {"st_sp_n",imm, },
 
-	ADD : {"add", true, },
-	SUB : {"sub", true, },
-	MUL : {"mul", true, },
-	MOD : {"mod", true, },
-	DIV : {"div", true, },
-	INC : {"inc", true, },
-	DEC : {"dec", true, },
-	CMP : {"cmp", true, },
-	AND : {"and", true, },
-	OR  : {"or", true, },
-	NOT : {"not", true, },
+	ADD : {"add", addr, },
+	SUB : {"sub", addr, },
+	MUL : {"mul", addr, },
+	MOD : {"mod", addr, },
+	DIV : {"div", addr, },
+	INC : {"inc", none, },
+	DEC : {"dec", none, },
+	CMP : {"cmp", addr, },
+	AND : {"and", addr, },
+	OR  : {"or", addr, },
+	NOT : {"not", none, },
 
-	IN  : {"in", true, },
-	OUT : {"out", true, },
+	IN  : {"in", port, },
+	OUT : {"out", port, },
 	
 }
-
-var RegisterTable = map[Register]uint32 {
-	
-}
-
 
 type Instruction struct {
 	Opcode
 	Operand
 }
 
-type IntVector struct {
-	
-}
+
 

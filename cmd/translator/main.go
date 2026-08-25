dd9de526b822f9300b9d8c07d12692c1b6791269
Naswek <nastya.rovkova06@gmail.com>
2026-08-25 14:25:48 +0300

    add: main func for translator

---
+++ cmd/translator/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"cpu-simulator/internal/isa"
+	"cpu-simulator/internal/translator"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+
+	if len(os.Args) != 3 {
+		log.Fatal("not enough args\nusage: translator <input.forth> <output.bin>")
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	bytes, err := os.ReadFile(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	source := string(bytes)
+
+	instructions, err := translator.Translate(source)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := isa.EncodeProgram(instructions)
+	err = isa.WriteProgram(outputFile, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = isa.WriteDisasm(makeDisasm(outputFile), data)
+	if err != nil {
+		log.Fatalf("something went wrong with disasm log: %v", err)
+	}
+}
+
+func makeDisasm(outputFile string) string {
+	ext := filepath.Ext(outputFile)
+	return strings.TrimSuffix(outputFile, ext) + ".disasm"
+}

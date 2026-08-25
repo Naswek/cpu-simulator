4de4b8a3b6d33e81ce99a72bdd7c192073eda60d
Naswek <nastya.rovkova06@gmail.com>
2026-08-26 02:41:25 +0300

    add: first version of control flow

---
+++ internal/translator/control.go
@@ -0,0 +1,24 @@
+package translator
+
+import (
+	"cpu-simulator/internal/isa"
+)
+
+type ControlFrame struct {
+	kind ControlKind
+	jumpIndex int
+	startAddr isa.Operand
+}
+
+type ControlKind uint8
+const (
+	If ControlKind = iota
+)
+
+func (t *Translator) currentAddr() uint32 {
+	return uint32(len(t.code)) * isa.WordSize
+}
+
+func (t *Translator) patchOperand(index int, addr isa.Operand) {
+	t.code[index].Operand = addr
+}

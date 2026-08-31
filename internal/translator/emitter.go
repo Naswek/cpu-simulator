d3b96f484ee3f6b04acc7196cb508e465ef953b6
Naswek <nastya.rovkova06@gmail.com>
2026-08-31 23:39:27 +0300

    update: little translator's refactors

--- internal/translator/emitter.go
+++ internal/translator/emitter.go
@@ -9,17 +9,6 @@ type scratch struct {
 	tmpAddr isa.Operand
 }
 
-type printRuntime struct {
-	entryAddr          isa.Operand
-	valueAddr          isa.Operand
-	divisorAddr        isa.Operand
-	digitAddr          isa.Operand
-	startedAddr        isa.Operand
-	initialDivisorAddr isa.Operand
-	tenAddr            isa.Operand
-	asciiZeroAddr      isa.Operand
-}
-
 type Translator struct {
 	code                   []isa.Instruction
 	mainScratch            scratch
@@ -154,74 +143,6 @@ func (t *Translator) emitKey() {
 func (t *Translator) emitEmit() {
 	t.emitDrop()
 	t.emit(isa.OUT, isa.Operand(isa.PortOutput))
-}
-
-func (t *Translator) emitPrintNumber() {
-	t.emitCall(t.printRuntime.entryAddr)
-}
-
-func (t *Translator) emitPrintNumberRoutine() {
-	rt := &t.printRuntime
-	rt.entryAddr = isa.Operand(t.currentAddr())
-
-	t.emitDrop()
-	t.emit(isa.ST_ADDR, rt.valueAddr)
-	t.emit(isa.CMP, t.zeroAddr)
-	zeroJumpIndex := len(t.code)
-	t.emit(isa.JZ, 0)
-
-	t.emit(isa.LD_ADDR, rt.initialDivisorAddr)
-	t.emit(isa.ST_ADDR, rt.divisorAddr)
-	t.emit(isa.LD_IMM, 0)
-	t.emit(isa.ST_ADDR, rt.startedAddr)
-
-	loopAddr := t.currentAddr()
-	t.emit(isa.LD_ADDR, rt.divisorAddr)
-	t.emit(isa.CMP, t.zeroAddr)
-	doneJumpIndex := len(t.code)
-	t.emit(isa.JZ, 0)
-
-	t.emit(isa.LD_ADDR, rt.valueAddr)
-	t.emit(isa.DIV, rt.divisorAddr)
-	t.emit(isa.ST_ADDR, rt.digitAddr)
-
-	t.emit(isa.LD_ADDR, rt.digitAddr)
-	t.emit(isa.CMP, t.zeroAddr)
-	printJumpIndex := len(t.code)
-	t.emit(isa.JNZ, 0)
-
-	t.emit(isa.LD_ADDR, rt.startedAddr)
-	t.emit(isa.CMP, t.zeroAddr)
-	skipPrintJumpIndex := len(t.code)
-	t.emit(isa.JZ, 0)
-
-	printAddr := t.currentAddr()
-	t.patchOperand(printJumpIndex, printAddr)
-	t.emit(isa.LD_ADDR, rt.digitAddr)
-	t.emit(isa.ADD, rt.asciiZeroAddr)
-	t.emit(isa.OUT, isa.Operand(isa.PortOutput))
-	t.emit(isa.LD_IMM, 1)
-	t.emit(isa.ST_ADDR, rt.startedAddr)
-
-	skipPrintAddr := t.currentAddr()
-	t.patchOperand(skipPrintJumpIndex, skipPrintAddr)
-	t.emit(isa.LD_ADDR, rt.valueAddr)
-	t.emit(isa.MOD, rt.divisorAddr)
-	t.emit(isa.ST_ADDR, rt.valueAddr)
-	t.emit(isa.LD_ADDR, rt.divisorAddr)
-	t.emit(isa.DIV, rt.tenAddr)
-	t.emit(isa.ST_ADDR, rt.divisorAddr)
-	t.emit(isa.JMP, isa.Operand(loopAddr))
-
-	doneAddr := t.currentAddr()
-	t.patchOperand(doneJumpIndex, doneAddr)
-	t.emitReturn()
-
-	zeroAddr := t.currentAddr()
-	t.patchOperand(zeroJumpIndex, zeroAddr)
-	t.emit(isa.LD_ADDR, rt.asciiZeroAddr)
-	t.emit(isa.OUT, isa.Operand(isa.PortOutput))
-	t.emitReturn()
 }
 
 func (t *Translator) emitFetch() {
